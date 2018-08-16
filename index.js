8374398571bae997cdd8fef25940c16bc8117d65
Sean Tayler <seanjtayler@gmail.com>
2018-08-16 09:52:36 -0700

    sean's add

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+
+
 (function() {
   'use strict';
+
+let stopBtn = document.getElementById('stopButton');
+let slowBtn = document.getElementById('slowButton');
+let goBtn = document.getElementById('goButton');
+
+let stopLight = document.getElementById('stopLight');
+let slowLight = document.getElementById('slowLight');
+let goLight = document.getElementById('goLight');
+
+stopBtn.addEventListener('click', () => stopLight.classList.add('stop')); 
+
+
+
 
   // YOUR CODE HERE
 })();
