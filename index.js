71ab57d25c6b27f54ee00e456be88aec87d9c4cd
Sean Tayler <seanjtayler@gmail.com>
2018-08-16 10:10:39 -0700

    Mike commit

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ slowBtn.addEventListener('click',() => slowLight.classList.toggle('slow'));
 goBtn.addEventListener('click',()=> goLight.classList.toggle('go'));
 
 
+stopBtn.addEventListener('mouseenter', () => console.log("Entered stop button."));
+stopBtn.addEventListener('mouseleave', () => console.log("Left stop button."));
+
+
+slowBtn.addEventListener('mouseenter', () => console.log("Entered slow button."));
+slowBtn.addEventListener('mouseleave', () => console.log("Left slow button."));
+
+
+goBtn.addEventListener('mouseenter', () => console.log("Entered go button."));
+goBtn.addEventListener('mouseleave', () => console.log("Left go button."));
+
+
 
   // YOUR CODE HERE
 })();
