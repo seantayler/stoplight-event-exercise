30db99fe6d51d4fee89c0d365dcce65c6d8d75a7
Sean Tayler <seanjtayler@gmail.com>
2018-08-16 10:02:59 -0700

    Chelsea's add

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ let stopLight = document.getElementById('stopLight');
 let slowLight = document.getElementById('slowLight');
 let goLight = document.getElementById('goLight');
 
-stopBtn.addEventListener('click', () => stopLight.classList.add('stop')); 
-
+stopBtn.addEventListener('click', () => stopLight.classList.toggle('stop'));
+slowBtn.addEventListener('click',() => slowLight.classList.toggle('slow'));
+goBtn.addEventListener('click',()=> goLight.classList.toggle('go'));
 
 
 
